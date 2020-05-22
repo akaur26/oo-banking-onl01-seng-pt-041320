34b6dedef5abb7bf6cab2f2b192f1ab47db17b40
Anmol Kaur <kaur.anmol26@gmail.com>
2020-05-22 04:02:00 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -12,21 +12,7 @@ class Transfer
     @sender.valid? && @receiver.valid? ? true : false
   end
 
-  def execute_transaction 
-    if both_valid? && status == "pending"
-      if amount < self.sender.balance
-        self.sender.balance -= amount
-        self.receiver.balance += amount
-        self.status = "complete"
-      else
-         self.status = "rejected"
-         "Transaction rejected. Please check your account balance."
-      end
-    else
-       self.status = "rejected"
-       "Transaction rejected. Please check your account balance."
-    end
-  end
+  
 
   def reverse_transfer
     if @status == "complete"
